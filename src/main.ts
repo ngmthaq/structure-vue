c6a28c1869dcf389aef38a598ef89116ed6810d2
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-04-15 17:11:22 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
+/**
+ * Import packages area
+ */
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+/**
+ * Import built-in modules | components
+ */
 import App from "./App.vue";
 import router from "./router";
 
+/**
+ * Import static files | stylesheets
+ */
 import "./assets/main.css";
 
+/**
+ * Create application
+ */
 const app = createApp(App);
 
+/**
+ * Apply plugins
+ */
 app.use(createPinia());
 app.use(router);
 
+/**
+ * Mount application
+ */
 app.mount("#app");
