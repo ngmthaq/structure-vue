42b0ae8bfeb4bfcf6848778615357f9bfd2623e6
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-04-24 11:39:06 +0700

    Update timeline

--- src/components/home/HomeExperiences.vue
+++ src/components/home/HomeExperiences.vue
@@ -12,14 +12,31 @@
         </v-row>
         <v-row>
           <v-col cols="12">
-            <p class="text-center px-5">
+            <p class="text-center px-5 text-white">
               My résumé includes my learning process in university and my working experiences. I have a little bit of working experiences and I need your
               company to grow it up
             </p>
           </v-col>
         </v-row>
         <v-row class="mt-5">
-          <v-col cols="12"> </v-col>
+          <v-col cols="12">
+            <HomeTimelines
+              :data="[
+                {
+                  company: { name: `Thuong Mai University`, image: `/favicon.ico`, url: `#`, description: `Hihi hehe haha`, time: `24/04.2023` },
+                  actions: [
+                    {
+                      name: `Nguyen Manh Thang`,
+                      image: `/favicon.ico`,
+                      url: `#`,
+                      title: `Hihi hehe hahahaha`,
+                      label: `fa-solid fa-user`,
+                    },
+                  ],
+                },
+              ]"
+            />
+          </v-col>
         </v-row>
       </v-container>
     </div>
@@ -30,9 +47,12 @@
 import { CUSTOM_EVENTS } from "@/const/app.const";
 import { Component, defineClassComponent } from "@/core/component.core";
 import EventBus from "@/plugins/bus.plugin";
+import HomeTimelines from "./HomeTimelines.vue";
 
 const app = defineClassComponent(
   class HomeBanner extends Component {
+    public state = this.reactive({});
+
     public onOver = () => {
       EventBus.emit<string>(CUSTOM_EVENTS.onChangeTab, "experiences");
     };
@@ -46,7 +66,8 @@ const app = defineClassComponent(
 
 #experiences {
   width: 100%;
-  margin-top: 100px;
+  padding-top: 100px;
+  background: $dark;
 }
 
 .title-cont {
@@ -60,7 +81,7 @@ const app = defineClassComponent(
     text-transform: uppercase;
     margin-bottom: 20px;
     padding-bottom: 0;
-    color: $dark;
+    color: $white;
     margin-bottom: 0;
     position: absolute;
     bottom: 0;
@@ -75,7 +96,7 @@ const app = defineClassComponent(
     text-transform: uppercase;
     margin-bottom: 20px;
     padding-bottom: 0;
-    color: $gray;
+    color: $gray-light;
     position: absolute;
     top: 0;
     left: 0;
---
+++ src/components/home/HomeTimelines.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="timeline-container">
+    <div class="timeline" v-for="(item, index) in data" :key="index">
+      <div class="company-image-container">
+        <img :src="item.company.image" :alt="item.company.name" />
+      </div>
+      <div class="timeline-item">
+        <div class="company-description-container">
+          <div class="header">
+            <a target="_blank" :href="item.company.url">{{ item.company.name }}</a>
+            <p>commented at {{ item.company.time }}</p>
+          </div>
+          <div class="body">{{ item.company.description }}</div>
+        </div>
+        <div class="action-container">
+          <div class="action-item" v-for="(actionItem, actionIndex) in item.actions" :key="actionIndex">
+            <div class="line"></div>
+            <div class="item">
+              <div class="label">
+                <font-awesome-icon :icon="actionItem.label" />
+              </div>
+              <div class="data">
+                <img :src="actionItem.image" :alt="actionItem.name" />
+                <a :href="actionItem.url" target="_blank">{{ actionItem.name }}</a>
+                <p>{{ actionItem.title }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "@/core/component.core";
+
+type Props = {
+  data: Array<{
+    company: {
+      name: string;
+      image: string;
+      url: string;
+      description: string;
+      time: string;
+    };
+    actions: Array<{
+      name: string;
+      image: string;
+      url: string;
+      title: string;
+      label: string;
+    }>;
+  }>;
+};
+
+defineProps<Props>();
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+@import "@/assets/scss/mixins";
+
+.timeline-container {
+  & .timeline {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    & .company-image-container {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      overflow: hidden;
+      flex-shrink: 0;
+      margin-right: 8px;
+
+      & img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+
+    & .timeline-item {
+      width: 100%;
+
+      & .company-description-container {
+        width: 100%;
+
+        & .header {
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: flex-start;
+          background-color: #161b22;
+          border: 1px solid #586472;
+          padding: 8px 16px;
+          border-radius: 8px 8px 0 0;
+
+          & a {
+            margin-right: 4px;
+            color: $light;
+            text-decoration: none;
+            font-weight: 500;
+            font-size: 16px;
+          }
+
+          & p {
+            color: $gray;
+            font-size: 14px;
+          }
+        }
+
+        & .body {
+          color: $light;
+          font-weight: 400;
+          font-size: 14px;
+          border: 1px solid #586472;
+          border-top: unset;
+          padding: 8px 16px;
+          border-radius: 0 0 8px 8px;
+        }
+      }
+
+      & .action-container {
+        width: 100%;
+
+        & .action-item {
+          width: 100%;
+
+          & .line {
+            height: 20px;
+            width: 100%;
+            border-left: 2px solid $gray;
+            margin-left: 20px;
+          }
+
+          & .item {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+
+            & .label {
+              width: 36px;
+              height: 36px;
+              border-radius: 50%;
+              background-color: rgba($color: #0e0e0e, $alpha: 0.3);
+              margin-left: 2px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              color: $gray;
+              margin-right: 16px;
+            }
+
+            & .data {
+              display: flex;
+              align-items: center;
+              justify-content: flex-start;
+
+              & img {
+                width: 24px;
+                height: 24px;
+                border-radius: 50%;
+                margin-right: 4px;
+                object-fit: cover;
+              }
+
+              & a {
+                color: $light;
+                font-size: 14px;
+                text-decoration: none;
+              }
+
+              & p {
+                color: $gray;
+                font-size: 14px;
+                padding-left: 4px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
