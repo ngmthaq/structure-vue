1f356067408d19f7961ffc92bf962c8f15e4fb98
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-04-26 23:19:22 +0700

    Update helper

--- src/helpers/storage.helper.ts
+++ src/helpers/storage.helper.ts
@@ -1,37 +1,39 @@
 import Cookies from "js-cookie";
 import localforage from "localforage";
 
-export async function setLocalForage<T>(key: string, value: T): Promise<T> {
-  const output = await localforage.setItem<T>(key, value);
-  return output;
-}
-
-export async function getLocalForage<T>(key: string): Promise<T | null> {
-  const output = await localforage.getItem<T>(key);
-  return output;
-}
-
-export async function removeLocalForage(key: string): Promise<boolean> {
-  try {
-    await localforage.removeItem(key);
-    return true;
-  } catch (error) {
-    console.warn(error);
-    return false;
+export default class StorageHelper {
+  public static async setLocalForage<T>(key: string, value: T): Promise<T> {
+    const output = await localforage.setItem<T>(key, value);
+    return output;
   }
-}
 
-export function setCookies<T>(key: string, value: T, day: number = 0, options: Cookies.CookieAttributes = {}): void {
-  let data: any = value;
-  if (typeof value === "boolean") data = value === true ? 1 : 0;
-  Cookies.set(key, JSON.stringify(data), { ...options, expires: day });
-}
+  public static async getLocalForage<T>(key: string): Promise<T | null> {
+    const output = await localforage.getItem<T>(key);
+    return output;
+  }
 
-export function getCookies<T>(key: string): T | null {
-  const value = Cookies.get(key);
-  return value ? JSON.parse(value) : null;
-}
+  public static async removeLocalForage(key: string): Promise<boolean> {
+    try {
+      await localforage.removeItem(key);
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
 
-export function removeCookies(key: string, options: Cookies.CookieAttributes = {}): void {
-  Cookies.remove(key, options);
+  public static setCookies<T>(key: string, value: T, day: number = 0, options: Cookies.CookieAttributes = {}): void {
+    let data: any = value;
+    if (typeof value === "boolean") data = value === true ? 1 : 0;
+    Cookies.set(key, JSON.stringify(data), { ...options, expires: day });
+  }
+
+  public static getCookies<T>(key: string): T | null {
+    const value = Cookies.get(key);
+    return value ? JSON.parse(value) : null;
+  }
+
+  public static removeCookies(key: string, options: Cookies.CookieAttributes = {}): void {
+    Cookies.remove(key, options);
+  }
 }
--- src/plugins/api.plugin.ts
+++ src/plugins/api.plugin.ts
@@ -1,12 +1,12 @@
 import KeyConst from "@/const/key.const";
 import Fetch, { type HttpRequestInit } from "@/core/fetch.core";
-import { getCookies } from "@/helpers/storage.helper";
+import StorageHelper from "@/helpers/storage.helper";
 
 export class Api extends Fetch {}
 
 export class AuthApi extends Fetch {
   protected onBeforeSend(configs: HttpRequestInit): HttpRequestInit {
-    const token: string | null = getCookies(KeyConst.KEY_ACCESS_TOKEN);
+    const token: string | null = StorageHelper.getCookies(KeyConst.KEY_ACCESS_TOKEN);
     if (token) {
       configs.headers.append("Authorization", "Bearer " + token);
     } else {
