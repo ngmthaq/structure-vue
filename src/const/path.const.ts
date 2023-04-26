751a1c740db5c5ba0b16e7690918d32384f8c170
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-04-26 22:52:03 +0700

    Update dynamic components

--- src/const/path.const.ts
+++ src/const/path.const.ts
@@ -1,12 +1,11 @@
 import type { Routes } from "@/types/const";
-import HomeViewVue from "@/views/HomeView.vue";
 
 export default class PathConst {
   public static PATHS: Routes = {
     home: {
       path: "/",
       name: "home",
-      component: HomeViewVue,
+      component: () => import("@/views/HomeView.vue"),
       meta: {
         title: "Nguyen Manh Thang",
       },
