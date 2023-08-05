afb6fb52d154e43a7c72601cc37f7ce4a269dd12
Nguyen Manh Thang <nguyenmanhthang2000.fb@gmail.com>
2023-08-05 22:06:33 +0700

    Add Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import eslint from "vite-plugin-eslint";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,13 @@ export default defineConfig({
     eslint(),
     VueI18nPlugin({
       include: [path.resolve(__dirname, "./src/locales/**")],
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        sourcemap: true,
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg}"],
+      },
     }),
   ],
   resolve: {
